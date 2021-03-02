f0398a4db2fceccb02a1a3901dff5ab84a402992
notGonim <mhmodgonim96@gmail.com>
2021-03-02 06:36:10 +0200

    Added footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/footer.scss'
+
+export const Footer = () => {
+    return (
+        <div className="footer_container">
+            <p>Contest Calendar</p>
+            <p>Request a Feature</p>
+            <p>Careers</p>
+            <p>Support</p>
+            <p>About Us</p>
+            <p> FAQ</p>
+        </div>
+    )
+}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { MainComponent } from '../components/MainComponent'
 
@@ -7,6 +8,7 @@ export const Main = () => {
         <div className="main_container">
             <Header />
             <MainComponent />
+            <Footer />
         </div>
     )
 }
