1e1dfe983bdcd5d6896c5541e95c03b5522a8d54
notGonim <mhmodgonim96@gmail.com>
2021-03-02 04:49:27 +0200

    Added the main files

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAD6w-5NGBGF000G6gEe3juu2Y9y2Yd8oM",
+    authDomain: "deve-28f9b.firebaseapp.com",
+    projectId: "deve-28f9b",
+    storageBucket: "deve-28f9b.appspot.com",
+    messagingSenderId: "719897519344",
+    appId: "1:719897519344:web:81e42b9359b51d861a8e35"
+};
+
