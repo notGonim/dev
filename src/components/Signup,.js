09aeb93667d3faa95a4293ec719214d6d3dd88be
notGonim <mhmodgonim96@gmail.com>
2021-03-04 06:40:16 +0200

    Added loginc to sign up

--- src/components/Signup,.js
+++ src/components/Signup,.js
@@ -2,7 +2,16 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import '../styles/signup.scss'
 export const Signup = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [profilePic, setProfilePic] = useState("")
+    const dispatch = useDispatch()
     let history = useHistory()
+
+
+
 
     const createAccountLink = () => {
         history.push("/login");
@@ -15,10 +24,14 @@ export const Signup = () => {
                 <p>Join Us and Enjoy Sharing your Experience with other developers </p>
             </div>
             <form >
-                <input type="text" placeholder="Your Username" />
-                <input type="email" placeholder="Your email" />
-                <input type="text" placeholder="Photo URL" />
-                <input type="password" placeholder="Your password" />
+                <input type="text" placeholder="Your Username" value={name}
+                    onChange={(e) => setName(e.target.value)} />
+                <input type="email" placeholder="Your email" value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                <input type="text" placeholder="Photo URL" value={profilePic}
+                    onChange={(e) => setProfilePic(e.target.value)} />
+                <input type="password" placeholder="Your password" value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
                 <button>Sign Up</button>
                 <p>If you  have an account <span onClick={createAccountLink}>Log In </span></p>
             </form>
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Dashboard = () => {
+    return (
+        <div>
+            <h1>working</h1>
+        </div>
+    )
+}
